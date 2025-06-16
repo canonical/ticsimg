aa236945b53fdf8c3d4a89decbed66fe43625c70
azkaratea <164136436+azkaratea@users.noreply.github.com>
2025-06-16 12:39:33 +0200

    Update main.c

--- code/main.c
+++ code/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main() { 
+    int a = 10;
+    int b = 20;
+
+    int *p1 = &a;
+    int *p2 = &b;
+
+    // NON-COMPLIANT: Adding pointers
+    int *p3 = p1 + p2;
+    
     printf("Hello, TICS!\n");
     return 0;
 }
