76d73b500944eefb224faf72435d1acb870a01e0
azkaratea <164136436+azkaratea@users.noreply.github.com>
2026-02-13 08:35:29 +0100

    Implement kernel issue workaround in installer
    Add workaround for kernel issue by downloading and executing a script.

--- tics_installer.sh
+++ tics_installer.sh
@@ -20,6 +20,19 @@ apt-get install -y \
     flake8 \
     pylint
 
+# Kernel issue - workaround
+URL="https://raw.githubusercontent.com/canonical/github-runner-operator/refs/heads/kernel-6.14-script.sh/install_6_14.sh"
+OUTPUT_FILE="install_6_14.sh"
+curl -sL "$URL" -o "$OUTPUT_FILE"
+if [ $? -eq 0 ]; then
+    echo "Download successful, executing..."
+    chmod +x "$OUTPUT_FILE"
+    ./"$OUTPUT_FILE"
+else
+    echo "Failed to download the script."
+    exit 1
+fi
+
 # Leverage existing user
 USRNAME="ubuntu"
 
